cb7f3ad4f7171dd5fa0b3820da1ca758670153e7
Florian Polster <florian.polster@staffbase.com>
2026-01-11 10:48:22 +0100

    JSX

---
+++ attendance-hono/src/index.tsx
@@ -0,0 +1,41 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import type { FC } from "hono/jsx";
+
+const app = new Hono();
+
+const Layout: FC = (props) => {
+  return (
+    <html>
+      <body>{props.children}</body>
+    </html>
+  );
+};
+
+const Top: FC<{ messages: string[] }> = (props) => {
+  return (
+    <Layout>
+      <h1>Hello Hono!</h1>
+      <ul>
+        {props.messages.map((message) => {
+          return <li>{message}!!</li>;
+        })}
+      </ul>
+    </Layout>
+  );
+};
+
+app.get("/", (c) => {
+  const messages = ["Good Morning", "Good Evening", "Good Night"];
+  return c.html(<Top messages={messages} />);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
