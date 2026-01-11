c011397b9d95b57fecb4e44825d3862e4286f62b
Florian Polster <florian.polster@staffbase.com>
2026-01-11 15:46:47 +0100

    Add HTMX

--- attendance-hono/src/index.tsx
+++ attendance-hono/src/index.tsx
@@ -7,6 +7,9 @@ const app = new Hono();
 const Layout: FC = (props) => {
   return (
     <html>
+      <head>
+         <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.8/dist/htmx.min.js"></script>
+      </head>
       <body>{props.children}</body>
     </html>
   );
@@ -21,6 +24,7 @@ const Top: FC<{ messages: string[] }> = (props) => {
           return <li>{message}!!</li>;
         })}
       </ul>
+      <button hx-get="/hello" hx-target="body" hx-swap="afterend">Load</button>
     </Layout>
   );
 };
@@ -28,6 +32,10 @@ const Top: FC<{ messages: string[] }> = (props) => {
 app.get("/", (c) => {
   const messages = ["Good Morning", "Good Evening", "Good Night"];
   return c.html(<Top messages={messages} />);
+});
+
+app.get("/hello", (c) => {
+  return c.html(<p>"Hello Hono!"</p>);
 });
 
 serve(
